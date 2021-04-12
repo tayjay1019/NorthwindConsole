e3621784d0e9913388825202e1ffbe37208a71b1
BIT student <bitstudent@bitnet.wctc.edu>
2021-04-12 14:06:54 -0500

    first commit

---
+++ Model/Categories.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace NorthwindConsole.Model
+{
+    public partial class Categories
+    {
+        public Categories()
+        {
+            Products = new HashSet<Products>();
+        }
+
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Oi - Enter the name!")]
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Products> Products { get; set; }
+    }
+}
